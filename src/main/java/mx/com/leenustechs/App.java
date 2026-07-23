3daf35e4011bf64d2e528631805d92d12ed10cfb
Renee <reneleermz_@live.com.mx>
2026-07-22 19:53:05 -0600

    FEAT JPA

--- src/main/java/mx/com/leenustechs/App.java
+++ src/main/java/mx/com/leenustechs/App.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@EnableJpaRepositories(basePackages = "mx.com.leenustechs.ciaState")
 @EnableFeignClients(basePackages = "mx.com.leenustechs.ciaState")
 @ComponentScan(basePackages = "mx.com.leenustechs.ciaState")
 @SpringBootApplication
